87a411f436591481e3524610293174a9f52232b7
AhmetKbpc <resmi.ahmetk@gmail.com>
2022-05-17 10:50:24 +0300

    Core eklendi.

---
+++ CombiSystems.Core/Dtos/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombiSystems.Core.Dtos;
+
+public class CategoryDto : BaseDto<int>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public IList<ProductDto>? Products { get; set; }
+}
---
+++ CombiSystems.Core/Dtos/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombiSystems.Core.Dtos;
+
+public class ProductDto : BaseDto<Guid>
+{
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int CategoryId { get; set; }
+
+    public CategoryDto? Category { get; set; }
+}
