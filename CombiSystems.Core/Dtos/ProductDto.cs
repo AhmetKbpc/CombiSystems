9d5d481b65888dafd1b15db62d4ad65721caec6e
AhmetKbpc <resmi.ahmetk@gmail.com>
2022-05-17 11:17:02 +0300

    Combining OK.

--- CombiSystems.Core/Dtos/ProductDto.cs
+++ CombiSystems.Core/Dtos/ProductDto.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CombiSystems.Core.Dtos.Abstracts;
+
 
 namespace CombiSystems.Core.Dtos;
 
