819671c9c279d295afa2982bc1625dcf5080bba4
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-23 10:26:35 +0300

    Roles OK

--- CombiSystems.Web/Models/Role/Roles.cs
+++ CombiSystems.Web/Models/Role/Roles.cs
@@ -2,15 +2,7 @@
 {
     public class Roles
     {
-        public static readonly string Admin = "Admin";
-        public static readonly string Technician = "Technician";
-        public static readonly string Operator = "Operator";
-        public static readonly string User = "User";
-
-        public static List<string> RoleList = new List<string>()
-        {
-            Admin,Technician,Operator,User
-        };
+        
 
     }
 }
